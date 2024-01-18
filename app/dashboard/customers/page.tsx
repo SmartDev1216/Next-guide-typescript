e260edb5e6a6f089ffda7d6a85014613e5b8c3e8
smartdev <smartdev1216@gmail.com>
2024-01-18 15:58:02 -0500

    set basic next.js project structure

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+export default function Customers() {
+    return (
+        <div>
+            <p>
+                Customers page
+            </p>
+        </div>
+    )
+
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+export default function Invoices() {
+    return (
+        <div>
+            <p>
+                Invoices Page
+            </p>
+        </div>
+    )
+}
