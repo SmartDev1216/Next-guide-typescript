7fe494a1ddbc046578174b377b1a53c728a456de
smartdev <smartdev1216@gmail.com>
2024-02-01 22:26:53 -0800

    add edit validate implement

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -36,6 +36,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -47,6 +48,11 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id = 'customer-error' aria-live = "polite" aria-atomic="true">
+            {state.errors?.customerId&&state.errors.customerId.map((error:string)=>(
+              <p className='mt-2 text-sm text-red-500' key={error}>{error}</p>
+            ))}
           </div>
         </div>
 
@@ -65,9 +71,15 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <div id = 'amount-error' aria-live = "polite" aria-atomic="true">
+            {state.errors?.amount&&state.errors.amount.map((error:string)=>(
+              <p className='mt-2 text-sm text-red-500' key={error}>{error}</p>
+            ))}
+          </div>
           </div>
         </div>
 
@@ -77,8 +89,8 @@ export default function EditInvoiceForm({
             Set the invoice status
           </legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
-              <div className="flex items-center">
+            <div className="flex gap-4" aria-describedby="status-error">
+              <div className="flex items-center" >
                 <input
                   id="pending"
                   name="status"
@@ -110,6 +122,11 @@ export default function EditInvoiceForm({
                   Paid <CheckIcon className="h-4 w-4" />
                 </label>
               </div>
+              <div id = 'status-error' aria-live = "polite" aria-atomic="true">
+            {state.errors?.status&&state.errors.status.map((error:string)=>(
+              <p className='mt-2 text-sm text-red-500' key={error}>{error}</p>
+            ))}
+          </div>
             </div>
           </div>
         </fieldset>
